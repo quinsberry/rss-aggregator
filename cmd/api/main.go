08bbc37794a28f2c39693e33d1801ede3589efe7
ekolesnyk <eugeniusz.kolesnyk@gmail.com>
2024-03-16 01:29:50 +0100

    added main.go

---
+++ cmd/api/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/quinsberry/rss-aggregator/internal/config"
+	"github.com/quinsberry/rss-aggregator/internal/pkg/rss"
+	"github.com/quinsberry/rss-aggregator/internal/router"
+)
+
+func main() {
+	godotenv.Load()
+
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	cfg := config.NewApiConfig(dbUrl)
+	r := router.NewRouter(cfg)
+
+	go rss.StartScraping(cfg.DB, 10, time.Minute)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", portStr),
+		Handler: r,
+	}
+
+	log.Printf("Server is running on port %s", portStr)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal("Server is shutting down")
+	}
+}
